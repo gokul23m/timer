ca636c3ef67514bdfae86eac148b423895c03cdf
gokul23m <gokul23m2001@gmail.com>
2022-10-09 10:11:58 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+
+function Tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}
+setInterval(Tick, 1000);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Tick />);
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
